bc5b8f81f0a83f5768265c28a5afa4bf5cd60e9f
yjailbir <nkudashko@mail.ru>
2025-12-01 22:54:33 +0300

    lil fix

--- common/src/main/java/ru/yjailbir/commonslib/configuration/CommonConfiguration.java
+++ common/src/main/java/ru/yjailbir/commonslib/configuration/CommonConfiguration.java
@@ -1,6 +1,5 @@
 package ru.yjailbir.commonslib.configuration;
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class CommonConfiguration {
     @Bean
     @LoadBalanced
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder
-                .errorHandler(response -> false)
-                .build();
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
